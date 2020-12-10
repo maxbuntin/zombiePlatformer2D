899506940a7ac7f08408567cd472fa6436a20fa9
Max Buntin <44224523+nimbusHAPPY@users.noreply.github.com>
2020-12-10 10:53:47 +0200

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    
+    private void Start()
+    {
+        float width = gameObject.GetComponent<Camera>().aspect * (gameObject.GetComponent<Camera>().orthographicSize * 2);
+
+    }
+
+    void Update()
+    {
+        if (player.transform.position.x >= transform.position.x) {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
